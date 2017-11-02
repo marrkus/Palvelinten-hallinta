55d7d5c2f3f9a3041bd03f9e7d57789e12118d0f
Markus Nissinen <markus.nissinen42@gmail.com>
2017-11-02 13:36:12 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,25 @@
+echo "***************************"
+echo " "
+echo "Hello $USER"
+echo " "
+echo "***************************"
+sudo timedatectl set-timezone Europe/Helsinki
+setxkbmap fi
+sudo apt update
+sudo apt install -y git tree puppet gimp
+
+git clone https://github.com/marrkus/ssh-for-puppet.git
+
+cd ssh-for-puppet/Puppet/modules
+sudo cp -r ssh/ /etc/puppet/modules/
+cd /etc/puppet/
+
+sudo puppet apply --modulepath modules/ -e 'class {"ssh":}'
+
+cd
+
+echo "***************************"
+echo " "
+echo "Ready to use"
+echo " "
+echo "***************************"
